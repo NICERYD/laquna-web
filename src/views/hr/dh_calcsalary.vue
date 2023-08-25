7c6777396819459872907d2ba4ffa793a339b0d7
home <home@DESKTOP-RFO23JO.mshome.net>
2023-08-25 18:05:06 +0900

    DH 인사관리 - 급여표(6쪽) 엑셀다운로드 추가

--- src/views/hr/dh_calcsalary.vue
+++ src/views/hr/dh_calcsalary.vue
@@ -488,7 +488,7 @@ export default {
     <v-row>
       <v-col cols="4"></v-col>
       <v-col cols="2">
-        <v-btn color="primary" flat width="150" height="40">
+        <v-btn color="primary" flat width="150" height="40" @click="downloadReport('Payroll6InTable')">
           <span class="text-16">급여표(6쪽)</span></v-btn>
       </v-col>
       <v-col cols="2">
