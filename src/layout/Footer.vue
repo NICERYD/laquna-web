ef71005f14eb40b73893eee4979e29fade5c1cf1
aaa0742 <137038424+aaa0742@users.noreply.github.com>
2023-10-25 13:32:53 +0900

    footer 문구 수정

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -3,7 +3,7 @@
     <base-card
       class="d-flex flex-column text-center text-sm-left flex-sm-row justify-space-between align-center"
     >
-      <div class="text-subtitle-2 text-secondary my-3">© UI Lib 2021-2022</div>
+      <div class="text-subtitle-2 text-secondary my-3">디에이치 BI ©디에이치 corp. All Rights RESERVED</div>
       <!-- <v-btn
         href="https://store.vuetifyjs.com/products/Octavia-vuetify-3-admin-pro"
         color="primary"
